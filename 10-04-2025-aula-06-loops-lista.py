65afc378d6218229fa91d3cb63dc3d56332dcab9
UniFanor <UniFanor@FNS-WS-038922.EDUC.ACAD>
2025-04-10 20:40:37 -0300

    aula1004

---
+++ 10-04-2025-aula-06-loops-lista.py
@@ -0,0 +1,6 @@
+# Neste programa, iremos demostrar aplicação básica do laço ou loop for
+#for paulo in range (10):
+#   print("Eu sou o Paulo")
+lista = ["Paulo","Davi","Jose","Antonio","Ze"]
+#for nome in lista:
+print(lista[2])
